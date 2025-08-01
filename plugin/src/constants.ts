acbe600ea2583ed0d484f6990a0a460029967939
iSparkiy <121734272+iSparkiy@users.noreply.github.com>
2025-08-01 00:25:44 -0600

    Minor fixes to packages

--- plugin/src/constants.ts
+++ plugin/src/constants.ts
@@ -1,3 +1,4 @@
 export const COMPONENT_PREFIX = "_compo_component:";
 export const COMPONENT_ADDED_FLAG = "_compo_added_flag";
 export const COMPONENT_REMOVED_FLAG = "_compo_removed_flag";
+export const COMPONENT_ACTIVE = "_compo_active";
